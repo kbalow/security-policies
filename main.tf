898b92496e728d70f5469cc77644222af40e4475
kbalow <kbalow@gmail.com>
2021-04-29 13:44:43 -0500

    interpolation changes

--- main.tf
+++ main.tf
@@ -18,92 +18,92 @@ variable "tfe_organization" {
 }
 
 provider "tfe" {
-  hostname = "${var.tfe_hostname}"
-  token    = "${var.tfe_token}"
+  hostname = var.tfe_hostname
+  token    = var.tfe_token
   version  = "~> 0.6"
 }
 
 data "tfe_workspace_ids" "all" {
   names        = ["*"]
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
 }
 
 locals {
-  workspaces = "${data.tfe_workspace_ids.all.ids}" # map of names to IDs
+  workspaces = data.tfe_workspace_ids.all.ids # map of names to IDs
 }
 
 resource "tfe_policy_set" "global" {
   name         = "global"
   description  = "Policies that should be enforced on ALL infrastructure."
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
   global       = true
 
   policy_ids = [
-    "${tfe_sentinel_policy.passthrough.id}",
-    "${tfe_sentinel_policy.aws-block-allow-all-cidr.id}",
-    "${tfe_sentinel_policy.aws-restrict-instance-type-default.id}",
-    "${tfe_sentinel_policy.aws-vpc-must-be-pmr-approved.id}",
-    "${tfe_sentinel_policy.limit-cost-by-workspace-type.id}",
-    "${tfe_sentinel_policy.limit-proposed-monthly-cost.id}",
-    "${tfe_sentinel_policy.restrict-cost-and-percentage-increase.id}",
+    tfe_sentinel_policy.passthrough.id,
+    tfe_sentinel_policy.aws-block-allow-all-cidr.id,
+    tfe_sentinel_policy.aws-restrict-instance-type-default.id,
+    tfe_sentinel_policy.aws-vpc-must-be-pmr-approved.id,
+    tfe_sentinel_policy.limit-cost-by-workspace-type.id,
+    tfe_sentinel_policy.limit-proposed-monthly-cost.id,
+    tfe_sentinel_policy.restrict-cost-and-percentage-increase.id,
   ]
 }
 
 resource "tfe_policy_set" "production" {
   name         = "production"
   description  = "Policies that should be enforced on production infrastructure."
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
 
   policy_ids = [
-    "${tfe_sentinel_policy.aws-restrict-instance-type-prod.id}",
-    #"${tfe_sentinel_policy.prod-change-window-hours.id}",
+    tfe_sentinel_policy.aws-restrict-instance-type-prod.id,
+    #tfe_sentinel_policy.prod-change-window-hours.id,
   ]
 
   workspace_ids = [
-    "${local.workspaces["Initech-production"]}",
+    local.workspaces["Initech-production"],
   ]
 }
 
 resource "tfe_policy_set" "development" {
   name         = "development"
   description  = "Policies that should be enforced on development or scratch infrastructure."
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
 
   policy_ids = [
-    "${tfe_sentinel_policy.aws-restrict-instance-type-dev.id}",
-    "${tfe_sentinel_policy.allowed-working-hours.id}",
+    tfe_sentinel_policy.aws-restrict-instance-type-dev.id,
+    tfe_sentinel_policy.allowed-working-hours.id,
   ]
 
   workspace_ids = [
-    "${local.workspaces["Initech-development"]}",
+    local.workspaces["Initech-development"],
   ]
 }
 
 resource "tfe_policy_set" "staging" {
   name         = "staging"
   description  = "Policies that should be enforced on staging environments."
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
 
   policy_ids = [
-    "${tfe_sentinel_policy.aws-restrict-instance-type-stage.id}",
+    tfe_sentinel_policy.aws-restrict-instance-type-stage.id,
   ]
 
   workspace_ids = [
-    "${local.workspaces["Initech-staging"]}",
+    local.workspaces["Initech-staging"],
   ]
 }
 
 resource "tfe_policy_set" "sentinel" {
   name         = "sentinel"
   description  = "Policies that watch the watchman. Enforced only on the workspace that manages policies."
-  organization = "${var.tfe_organization}"
+  organization = var.tfe_organization
 
   policy_ids = [
-    "${tfe_sentinel_policy.tfe_policies_only.id}",
+    tfe_sentinel_policy.tfe_policies_only.id,
   ]
 
   workspace_ids = [
-    "${local.workspaces["InitechSecurity"]}",
+    local.workspaces["InitechSecurity"],
   ]
 }
 
@@ -112,8 +112,8 @@ resource "tfe_policy_set" "sentinel" {
 resource "tfe_sentinel_policy" "passthrough" {
   name         = "passthrough"
   description  = "Just passing through! Always returns 'true'."
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./passthrough.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./passthrough.sentinel")
   enforce_mode = "advisory"
 }
 
@@ -122,8 +122,8 @@ resource "tfe_sentinel_policy" "passthrough" {
 resource "tfe_sentinel_policy" "tfe_policies_only" {
   name         = "tfe_policies_only"
   description  = "The Terraform config that manages Sentinel policies must not use the authenticated tfe provider to manage non-Sentinel resources."
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./tfe_policies_only.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./tfe_policies_only.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
@@ -132,24 +132,24 @@ resource "tfe_sentinel_policy" "tfe_policies_only" {
 resource "tfe_sentinel_policy" "aws-block-allow-all-cidr" {
   name         = "aws-block-allow-all-cidr"
   description  = "Avoid nasty firewall mistakes (AWS version)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-block-allow-all-cidr.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-block-allow-all-cidr.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
 resource "tfe_sentinel_policy" "azurerm-block-allow-all-cidr" {
   name         = "azurerm-block-allow-all-cidr"
   description  = "Avoid nasty firewall mistakes (Azure version)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./azurerm-block-allow-all-cidr.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./azurerm-block-allow-all-cidr.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
 resource "tfe_sentinel_policy" "gcp-block-allow-all-cidr" {
   name         = "gcp-block-allow-all-cidr"
   description  = "Avoid nasty firewall mistakes (GCP version)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./gcp-block-allow-all-cidr.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./gcp-block-allow-all-cidr.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
@@ -158,56 +158,56 @@ resource "tfe_sentinel_policy" "gcp-block-allow-all-cidr" {
 resource "tfe_sentinel_policy" "aws-restrict-instance-type-dev" {
   name         = "aws-restrict-instance-type-dev"
   description  = "Limit AWS instances to approved list (for dev infrastructure)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-restrict-instance-type-dev.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-restrict-instance-type-dev.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
 resource "tfe_sentinel_policy" "aws-restrict-instance-type-stage" {
   name         = "aws-restrict-instance-type-stage"
   description  = "Limit AWS instances to approved list (for dev infrastructure)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-restrict-instance-type-stage.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-restrict-instance-type-stage.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "aws-restrict-instance-type-prod" {
   name         = "aws-restrict-instance-type-prod"
   description  = "Limit AWS instances to approved list (for prod infrastructure)"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-restrict-instance-type-prod.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-restrict-instance-type-prod.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
 resource "tfe_sentinel_policy" "aws-restrict-instance-type-default" {
   name         = "aws-restrict-instance-type-default"
   description  = "Limit AWS instances to approved list"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-restrict-instance-type-default.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-restrict-instance-type-default.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "aws-vpc-must-be-pmr-approved" {
   name         = "aws-vpc-must-be-pmr-approved"
   description  = "Limit AWS VPC creation to pre-approved modules"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./aws-vpc-must-be-pmr-approved.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./aws-vpc-must-be-pmr-approved.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "azurerm-restrict-vm-size" {
   name         = "azurerm-restrict-vm-size"
   description  = "Limit Azure instances to approved list"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./azurerm-restrict-vm-size.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./azurerm-restrict-vm-size.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "gcp-restrict-machine-type" {
   name         = "gcp-restrict-machine-type"
   description  = "Limit GCP instances to approved list"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./gcp-restrict-machine-type.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./gcp-restrict-machine-type.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
@@ -215,16 +215,16 @@ resource "tfe_sentinel_policy" "gcp-restrict-machine-type" {
 resource "tfe_sentinel_policy" "allowed-working-hours" {
   name         = "allowed-working-hours"
   description  = "Only allow TF applies during specific working hours"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./working-hours.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./working-hours.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "prod-change-window-hours" {
   name         = "prod-change-window-hours"
   description  = "Only allow TF applies to prod environments during change window hours"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./change-window-hours.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./change-window-hours.sentinel")
   enforce_mode = "hard-mandatory"
 }
 
@@ -233,24 +233,24 @@ resource "tfe_sentinel_policy" "prod-change-window-hours" {
 resource "tfe_sentinel_policy" "limit-cost-by-workspace-type" {
   name         = "limit-cost-by-workspace-type.sentinel"
   description  = "Limit Workspace Cost by Type"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./limit-cost-by-workspace-type.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./limit-cost-by-workspace-type.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "limit-proposed-monthly-cost" {
   name         = "limit-proposed-monthly-cost.sentinel"
   description  = "Limit Proposed Monthly Cost"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./limit-proposed-monthly-cost.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./limit-proposed-monthly-cost.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
 resource "tfe_sentinel_policy" "restrict-cost-and-percentage-increase" {
   name         = "restrict-cost-and-percentage-increase.sentinel"
   description  = "Restrict Cost and Percentage Increase"
-  organization = "${var.tfe_organization}"
-  policy       = "${file("./restrict-cost-and-percentage-increase.sentinel")}"
+  organization = var.tfe_organization
+  policy       = file("./restrict-cost-and-percentage-increase.sentinel")
   enforce_mode = "soft-mandatory"
 }
 
